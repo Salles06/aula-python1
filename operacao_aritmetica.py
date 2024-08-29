b5dc44b13f48b2ca51c253b3e9fc3cb6914d4d07
Salles06 <joaovictorsalesdepaula@gmail.com>
2024-08-28 21:31:32 -0300

    operacao aritmetica

---
+++ operacao_aritmetica.py
@@ -0,0 +1,31 @@
+a = 5
+b = 3
+c = 10
+# Adição (+)
+resultado = a + b
+print(f'soma: {resultado}')
+
+#Subtração
+
+resultado = a - b
+print(f'subtracao: {resultado}')
+
+#multiplicação
+
+resultado = a * b
+print(f'multiplicacao: {resultado}')
+
+#Divisão Inteira
+
+resultado = a // b
+print(f'divisao Inteira: {resultado}')
+
+#Resto de Divisao
+
+resultado = a % b
+print(f'resto da divisao: {resultado}')
+
+#Exponencial(**)
+
+resultado = a ** b
+print(f'exponencial: {resultado}')
