5192a05db93902252cb916d90e803025d2f76bcd
Salles06 <joaovictorsalesdepaula@gmail.com>
2024-08-28 21:43:22 -0300

    Algoritimo compra

--- operacao_aritmetica.py
+++ operacao_aritmetica.py
@@ -29,3 +29,23 @@ print(f'resto da divisao: {resultado}')
 
 resultado = a ** b
 print(f'exponencial: {resultado}')
+
+##José foi ao mercado com R$ 50,00. Ele comprou:
+# - 2 refrigerantes R$ 8,00/cada
+# - 3 pães R$ 4,00
+# - 300g de mortadela ( Valor do quilo da mortadela R$ 39,90)
+# - Após as compras, José deseka saber quanto
+# - Sobrou de seu dinheiro e se há algum valor
+# - Exato que ele poderia gastar com os trocados restantes
+
+# Declarando Variavel
+
+saldoinicial = 50
+custoRefrigerante = 8
+custoPao = 4
+custoMortadela = 39.90
+
+valorcompra = (custoRefrigerante * 2) + custoPao + ((custoMortadela / 1000) * 300)
+
+saldofinal = saldoinicial - valorcompra
+print(f'Jose chegou com R$ {saldoinicial} e saiu com R$ {saldofinal}')
